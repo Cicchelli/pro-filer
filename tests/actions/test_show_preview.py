5e54f3e734eb6d26b516f5474f78e65b6463fe52
Stephan <cicchelli.stephan@gmail.com>
2024-03-26 15:03:09 -0300

    Req3

--- tests/actions/test_show_preview.py
+++ tests/actions/test_show_preview.py
@@ -1,1 +1,38 @@
-from pro_filer.actions.main_actions import show_preview  # NOQA
+import pytest
+from pro_filer.actions.main_actions import show_preview
+
+
+@pytest.mark.parametrize(
+    "context, expected_result",
+    [
+        (
+            {"all_files": [], "all_dirs": []},
+            "Found 0 files and 0 directories\n",
+        ),
+        (
+            {
+                "all_files": [
+                    "src/__init__.py",
+                    "src/app.py",
+                ],
+                "all_dirs": ["src", "src/utils"],
+            },
+            "Found 2 files and 2 directories\n"
+            "First 5 files: ['src/__init__.py', 'src/app.py']\n"
+            "First 5 directories: ['src', 'src/utils']\n",
+        ),
+    ],
+)
+def test_show_preview(capsys, context, expected_result):
+    """
+    Test function for show_preview.
+
+    Args:
+    - capsys: pytest fixture to capture stdout.
+    - context (dict): The context containing files and directories.
+    - expected_result (str): The expected output.
+    """
+    show_preview(context)
+
+    captured = capsys.readouterr()
+    assert captured.out == expected_result
